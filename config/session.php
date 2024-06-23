6de0f97c35ccedf48f52de7525f6bcbc66dec7f3
Abhishek G Jha <65995420+govind663@users.noreply.github.com>
2024-06-23 11:12:49 +0530

    LODHA | Seven-Wonders - 0.6

--- config/session.php
+++ config/session.php
@@ -169,7 +169,7 @@ return [
     |
     */
 
-    'secure' => env('SESSION_SECURE_COOKIE'),
+    'secure' => env('SESSION_SECURE_COOKIE',),
 
     /*
     |--------------------------------------------------------------------------
@@ -212,6 +212,6 @@ return [
     |
     */
 
-    'partitioned' => env('SESSION_PARTITIONED_COOKIE', false),
+    'partitioned' => env('SESSION_PARTITIONED_COOKIE', true),
 
 ];
